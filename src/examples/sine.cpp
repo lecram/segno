cf897b286ca03c52f797104f0c06beded89c2060
Marcel Rodrigues <marcelgmr@gmail.com>
2013-02-20 10:28:03 -0300

    Add comments to example.

--- src/examples/sine.cpp
+++ src/examples/sine.cpp
@@ -6,21 +6,31 @@
 int
 main()
 {
+    // Create two segno vertices:
+    //  A sine oscillator...
     Segno::Osc::Sine osc;
+    //  And a file writer.
     Segno::File::WriterMono writer("sine.flac", FORMAT);
+    // Define the frequency in Hertz.
     float frequency = 440.0f;
+    // Define the duration in seconds.
     float duration = 1.0f;
+
     unsigned int nsamples;
     unsigned int i;
 
+    // Make connections.
+    // A connection "a => b" is written as "b = &a;".
     osc.frequency = &frequency;
     writer.input = &osc.output;
 
+    // Tick all vertices in the graph to process signals.
     nsamples = duration * Segno::sample_rate;
     for (i = 0; i < nsamples; i++) {
         osc.tick();
         writer.tick();
     }
 
+    // Success. Note that the file is closed automatically.
     return 0;
 }
