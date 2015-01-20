c1b716d2fe1b0a7ca3044eea1a7476d32be1a5d2
Marcel Rodrigues <marcelgmr@gmail.com>
2015-01-20 16:31:45 -0200

    Remove TODO list.

--- segno.lua
+++ segno.lua
@@ -8,10 +8,3 @@ segno.dl   = require "dl"
 segno.fx   = require "fx"
 segno.file = require "file"
 return segno
-
--- TODO
---  * add envelopes (ADSR, etc);
---  * add events;
---  * add JACK real-time audio API;
---  * add more effects;
---  * implement tick sequence automation (is it possible?);
